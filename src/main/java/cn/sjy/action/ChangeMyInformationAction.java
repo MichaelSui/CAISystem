16c2d89866c9ff405557c774124a87edd98c7f03
MichaelSui <suijiyuan1996@gmail.com>
2018-04-27 16:26:07 +0800

    修改个人信息功能实现。

--- src/main/java/cn/sjy/action/ChangeMyInformationAction.java
+++ src/main/java/cn/sjy/action/ChangeMyInformationAction.java
@@ -1,8 +1,81 @@
 package cn.sjy.action;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import cn.sjy.db.Information;
+import cn.sjy.utils.HibernateUtil;
+
 public class ChangeMyInformationAction {
+    private String userId;
+    private String name;
+    private String email;
+    private String phone;
+    private String qqOrTim;
+    private String wechat;
+
+    public String getUserId() {
+	return userId;
+    }
+
+    public void setUserId(String userId) {
+	this.userId = userId;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public String getEmail() {
+	return email;
+    }
+
+    public void setEmail(String email) {
+	this.email = email;
+    }
+
+    public String getPhone() {
+	return phone;
+    }
+
+    public void setPhone(String phone) {
+	this.phone = phone;
+    }
+
+    public String getQqOrTim() {
+	return qqOrTim;
+    }
+
+    public void setQqOrTim(String qqOrTim) {
+	this.qqOrTim = qqOrTim;
+    }
+
+    public String getWechat() {
+	return wechat;
+    }
+
+    public void setWechat(String wechat) {
+	this.wechat = wechat;
+    }
+
     public String execute() throws Exception {
 	// 修改个人信息。
+	Session session = HibernateUtil.getSession();
+	Transaction transaction = session.beginTransaction();
+	Information i = new Information();
+	i.setUserId(userId);
+	i.setName(name);
+	i.setEmail(email);
+	i.setPhone(phone);
+	i.setQqOrTim(qqOrTim);
+	i.setWechat(wechat);
+	session.update(i);
+	transaction.commit();
+	session.close();
 
 	return "success";
     }
